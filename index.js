16b9616a7568e83f8845c11fe4b375c172a36890
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-05-25 20:24:56 +0600

    Delete method for deleting room item

--- index.js
+++ index.js
@@ -120,6 +120,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete a room
+    app.delete('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await roomsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
     console.log(
